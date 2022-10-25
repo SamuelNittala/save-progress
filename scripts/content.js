9d80919d22515b5c4b2f47e5fcb7fb23ebd98afa
SamuelNittala <nittalasamuel7@gmail.com>
2022-10-25 17:04:56 +0530

    WIP: Add support for hotstar/in OTT

---
+++ scripts/content.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.action === "SET_EPISODE") {
+		console.log("message received to set episode", request.episode);
+
+		//FIND A BETTER WAY
+		setTimeout(() => {
+			const currentEpisode = (document.getElementsByClassName(
+				"resClass"
+			)[0].children[request.episode - 1].style.border = "3px solid red");
+			console.log(currentEpisode);
+		}, 500);
+	}
+	if (request.action === "SET_SEASON") {
+		console.log("message received to set season", request.season);
+		setTimeout(() => {
+			const currentSeason = (document.getElementsByClassName(
+				"slick-track"
+			)[1].children[request.season - 1].style.border = "3px solid red");
+			console.log(currentSeason);
+		}, 500);
+	}
+});
