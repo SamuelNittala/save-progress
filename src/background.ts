bbdf38082c471cb43f6d9c5f489f3d80978ca20d
SamuelNittala <nittalasamuel7@gmail.com>
2022-10-26 13:44:49 +0530

    WIP: add basic types, fix tsconfig.json errors.

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,6 @@
 import { handleHotStarTitle } from "./otts/hotstar.js";
-
-const GET_CURRENT_TAB = "get_current_tab";
-const STORE_VALUE = "store_value";
-const SPACE = " ";
 const EMPTY = "";
-const HYPHEN = "-";
-const EPISODE = "episode";
-const SEASON = "season";
-
 const HOTSTAR = "HOTSTAR";
-
 
 function getOtt(url: chrome.tabs.Tab["url"]) {
 	if (url) {
@@ -29,7 +20,6 @@ async function handleOTT(currentTab: chrome.tabs.Tab) {
 
 try {
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-		// read changeInfo data and do something with it (like read the url)
 		handleOTT(tab);
 	});
 } catch (err) {
--- src/otts/hotstar.ts
+++ src/otts/hotstar.ts
@@ -1,18 +1,19 @@
-const GET_CURRENT_TAB = "get_current_tab";
-const STORE_VALUE = "store_value";
 const SPACE = " ";
 const EMPTY = "";
 const HYPHEN = "-";
 const EPISODE = "episode";
 const SEASON = "season";
 
-export async function handleHotStarTitle(currentTab) {
+const SET_SEASON = "SET_SEASON";
+const SET_EPISODE = "SET_EPISODE";
 
-	function formatString(string) {
+export async function handleHotStarTitle(currentTab : chrome.tabs.Tab) {
+
+	function formatString(string : string) {
 		return string.replaceAll(SPACE, EMPTY).toLowerCase();
 	}
 
-	function isTitleValid(title) {
+	function isTitleValid(title : string) {
 		const formattedTitle = formatString(title);
 		return (
 			formattedTitle.search(EPISODE) !== -1 &&
@@ -21,18 +22,18 @@ export async function handleHotStarTitle(currentTab) {
 		);
 	}
 
-	function getSeriesName(url) {
+	function getSeriesName(url : string) {
 		return url.split("/")[5].replace("-", "_");
 	}
 
-	function getNumbersFromTitle(key, formattedTitle) {
+	function getNumbersFromTitle(key : string, formattedTitle : string) {
 		return formattedTitle
 			.slice(formattedTitle.search(key) + key.length)
 			.split(EMPTY)
 			.reduce(
 				(prev, curr) => {
 					if (!prev.hitCharFlag) {
-						return !isNaN(curr)
+						return Number(curr)
 							? { ...prev, value: `${prev.value}${curr}` }
 							: { ...prev, hitCharFlag: true };
 					}
@@ -42,89 +43,86 @@ export async function handleHotStarTitle(currentTab) {
 			).value;
 	}
 
-	function convertToSEKey(formattedTitle) {
+	function convertToSEKey(formattedTitle : string) {
 		const episode = getNumbersFromTitle(EPISODE, formattedTitle);
 		const season = getNumbersFromTitle(SEASON, formattedTitle);
 		return `S-${season}-E-${episode}`;
 	}
 
-	function getSeasonAndEpisode(storageSEKey) {
+	function getSeasonAndEpisode(storageSEKey : string) {
 		return {
 			season: storageSEKey.split(HYPHEN)[1],
 			episode: storageSEKey.split(HYPHEN)[3],
 		};
 	}
 
-	async function currentStoredValue(seriesName) {
+	async function currentStoredValue(seriesName : string) {
 		const res = await chrome.storage.local.get(seriesName);
 		return res[seriesName];
 	}
 
-	function isSeasonTab(url) {
+	function isSeasonTab(url : string) {
 		const urlToArray = url.split("/");
 		return urlToArray[7] === "seasons";
 	}
 
-	function getCurrentSeasonFromUrl(url) {
+	function getCurrentSeasonFromUrl(url : string) {
 		const urlToArray = url.split("/");
 		return urlToArray[8].split(HYPHEN)[1];
 	}
 
-	function isHomeTab(url) {
+	function isHomeTab(url : string) {
 		const urlToArray = url.split("/");
-		return urlToArray.length === 7 && !isNaN(urlToArray[6]);
+		return urlToArray.length === 7 && Number(urlToArray[6]);
 	}
 
-	if (isHomeTab(currentTab.url)) {
-		const currentStoredProgress = await currentStoredValue(
-			getSeriesName(currentTab.url)
-		);
-		const { season } = getSeasonAndEpisode(currentStoredProgress.key);
-		chrome.tabs.query(
-			{ active: true, lastFocusedWindow: true },
-			function (tabs) {
-				if (tabs[0].id) {
-					chrome.tabs.sendMessage(tabs[0].id, { action: "SET_SEASON", season });
+	if (currentTab.url && currentTab.title) {
+		if (isHomeTab(currentTab.url)) {
+			const currentStoredProgress = await currentStoredValue(
+				getSeriesName(currentTab.url)
+			);
+			const { season } = getSeasonAndEpisode(currentStoredProgress.key);
+			chrome.tabs.query(
+				{ active: true, lastFocusedWindow: true },
+				function (tabs) {
+					if (tabs[0].id) {
+						chrome.tabs.sendMessage(tabs[0].id, { action: SET_SEASON, season });
+					}
 				}
-			}
-		);
-	}
-
-	if (isSeasonTab(currentTab.url)) {
-		console.log(currentTab.url);
-		const currSeason = getCurrentSeasonFromUrl(currentTab.url);
-		const currentStoredProgress = await currentStoredValue(
-			getSeriesName(currentTab.url)
-		);
-		console.log(currentStoredProgress);
-		const { season, episode } = getSeasonAndEpisode(currentStoredProgress.key);
-
-		if (season === currSeason) {
-			console.log("hit");
-			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-				if (tabs[0].id) {
-					chrome.tabs.sendMessage(tabs[0].id, {
-						action: "SET_EPISODE",
-						episode,
-					});
-				}
-			});
+			);
 		}
 
-		console.log(currSeason, currentStoredProgress, "llb");
-	}
-	if (isTitleValid(currentTab.title)) {
-		const currentEpisodeSeason = convertToSEKey(formatString(currentTab.title));
-		const seriesName = getSeriesName(currentTab.url);
-		const objectToStore = {
-			[seriesName]: {
-				key: currentEpisodeSeason,
-				url: currentTab.url,
-			},
-		};
-		console.log(objectToStore, "obs");
-		chrome.storage.local.set(objectToStore, () => {
-			console.log(`set ${seriesName} to ${currentEpisodeSeason}`);
-		});
+		if (isSeasonTab(currentTab.url)) {
+			const currSeason = getCurrentSeasonFromUrl(currentTab.url);
+			const currentStoredProgress = await currentStoredValue(
+				getSeriesName(currentTab.url)
+			);
+			const { season, episode } = getSeasonAndEpisode(currentStoredProgress.key);
+
+			if (season === currSeason) {
+				chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+					if (tabs[0].id) {
+						chrome.tabs.sendMessage(tabs[0].id, {
+							action: SET_EPISODE,
+							episode,
+						});
+					}
+				});
+			}
+		}
+
+		if (isTitleValid(currentTab.title)) {
+			const currentEpisodeSeason = convertToSEKey(formatString(currentTab.title || ''));
+			const seriesName = getSeriesName(currentTab.url);
+			const objectToStore = {
+				[seriesName]: {
+					key: currentEpisodeSeason,
+					url: currentTab.url,
+				},
+			};
+			chrome.storage.local.set(objectToStore, () => {
+				console.log(`set ${seriesName} to ${currentEpisodeSeason}`);
+			});
+		}
 	}
 }
